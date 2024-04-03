13c734844b9d5ee16e99f2683eee508d642663c8
Alvaro S. P. Silva <alvaro123sps@gmail.com>
2024-04-02 23:15:36 -0300

    Adding a page title

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,13 @@
+import Head from "next/head";
 import "../styles/globals.scss";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Pricify Hub</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
